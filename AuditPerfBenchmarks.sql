0aba1a2efc92b9220c47f2eb93cc0155d56d3ca1
codexguy <33130668+codexguy@users.noreply.github.com>
2018-08-24 16:35:02 -0700

    Update AuditPerfBenchmarks.sql

--- AuditPerfBenchmarks.sql
+++ AuditPerfBenchmarks.sql
@@ -21,8 +21,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-For more details on the usage of this script, visit www.xskrape.com, in particular an article at: https://www.xskrape.com/home/article/Data-Change-Audit--Winning-on-Performance--and-XS-Tool-Suite
-
+For more details on the usage of this script, visit www.xskrape.com, in particular
+an article at: https://www.xskrape.com/home/article/Data-Change-Audit--Winning-on-Performance--and-XS-Tool-Suite
 */
 
 CREATE SCHEMA [History]
